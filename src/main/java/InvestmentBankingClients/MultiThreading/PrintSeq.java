bacd1361436baa70ca6ce31333fc66ea2e5fa7e7
praveen2jan <praveen1668301@gmail.com>
2021-01-14 16:40:52 +0530

    added relevant changes

--- src/main/java/InvestmentBankingClients/MultiThreading/PrintSeq.java
+++ src/main/java/InvestmentBankingClients/MultiThreading/PrintSeq.java
@@ -15,7 +15,7 @@ public class PrintSeq implements Runnable{
     public void run() {
         while (number < PRINT_NUMBERS_UPTO - 1) {
             synchronized (lock) {
-                while (number % 3 != remainder) { // wait for numbers other than remainder
+                while (number % 4 != remainder) { // wait for numbers other than remainder
                     try {
                         lock.wait();
                     } catch (InterruptedException e) {
@@ -30,16 +30,19 @@ public class PrintSeq implements Runnable{
     }
 
 
-    public static void main(String[] args) {PrintSeq runnable1=new PrintSeq(1);
+    public static void main(String[] args) {
+        PrintSeq runnable1=new PrintSeq(1);
         PrintSeq runnable2=new PrintSeq(2);
         PrintSeq runnable3=new PrintSeq(0);
+        PrintSeq runnable4= new PrintSeq(3);
 
     Thread t1=new Thread(runnable1,"T1");
     Thread t2=new Thread(runnable2,"T2");
     Thread t3=new Thread(runnable3,"T3");
-
+    Thread t4=new Thread(runnable4,"T4");
 		t1.start();
 		t2.start();
 		t3.start();
+		t4.start();
 }
 }
